96ecc45b7e74dfa8ed7c594adc1e221283eaaf99
Mikhail9090 <Mikhail_Churakov@epam.com>
2017-06-15 11:34:40 +0300

    version 3

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -4,21 +4,30 @@ import com.epam.pageobject.LoginPanel;
 import com.epam.pageobject.MainPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
+import ru.yandex.qatools.allure.annotations.*;
+
 import java.lang.reflect.Method;
+
+import static ru.yandex.qatools.allure.model.SeverityLevel.BLOCKER;
+import static ru.yandex.qatools.allure.model.SeverityLevel.CRITICAL;
 
 /**
  * Created by Mikhail_Churakov on 5/13/2017.
  */
+@Title("This is LOGIN TESTS!!")
 public class LoginTests extends TestBase{
     WebDriver driver;
     private static String screenPath = "screenshots\\Login Tests\\";
     @Parameters("browser")
+    @Step("we're executing our driver and opening the main page")
     @BeforeSuite
     public void setupLoginTests(String browser) {
         driver = setDriver(browser);
+        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getPageSource());
         navigateToMainPage();
     }
-
+    @Step("we're closing the instance")
     @AfterSuite
     public void closeLoginTests() {
         closeBrowser();
@@ -33,7 +42,10 @@ public class LoginTests extends TestBase{
     public void closeTest(Method method) {
         makeScreen(method, screenPath);
     }
-
+    @Title("This is a positive login!!")
+    @Description("here we checked valid credentials")
+    @Severity(BLOCKER)
+    @Features("Login functionality")
     @Test(dataProvider = "correctData", dataProviderClass = DataProviders.class)
     public void positiveLogin(String login, String password) {
         MainPage mainPage = MainPage.get(driver);
@@ -47,12 +59,17 @@ public class LoginTests extends TestBase{
         assertFalse(loginPanel.passwordField.isDisplayed());
         assertTrue(loginPanel.userNameLabel.isDisplayed());
     }
+    @Title("This is a negative login!!")
+    @Description("here we checked invalid credentials")
+    @Severity(CRITICAL)
+    @Features("Login functionality")
     @Test(dataProvider = "incorrectDataCsv", dataProviderClass = DataProviders.class)
     public void negativeLogin(String login, String password) {
         MainPage mainPage = MainPage.get(driver);
         LoginPanel loginPanel = LoginPanel.get(driver);
         waitElement(mainPage.loginDropDown);
         mainPage.loginDropDown.click();
+
         assertTrue(loginPanel.loginLabel.isDisplayed());
         typeTextIn(login,loginPanel.loginField);
         typeTextIn(password, loginPanel.passwordField);
